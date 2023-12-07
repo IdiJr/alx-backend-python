84d32811743917b2f8f2b16fd1773b09d0157eab
IdiJr <idialijnr@gmail.com>
2023-12-07 13:33:35 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Returns n as a string. """
+
+
+def to_str(n: float) -> str:
+    """ Description: Accepts a float n as argument and returns the string
+    representation of the float.
+    Parameters: n: float
+    """
+    return str(n)
