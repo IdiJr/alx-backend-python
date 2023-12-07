a93db0fd1048623b6b30798743f61875f9964773
IdiJr <idialijnr@gmail.com>
2023-12-07 11:53:00 +0000

    Basic annotations-floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Return largest int value less than or equal to n. """
+
+
+def floor(n: float) -> int:
+    """ Description:takes a float n
+    as argument and returns the floor of the float
+    Parameter: n: float
+    """
+    return int(n)
