89d4fa6e463b1b2cebe04c1d817390c1295c1503
IdiJr <idialijnr@gmail.com>
2023-12-12 00:00:50 +0000

    4. Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,23 +5,23 @@ Execute task_wait_random and returns sorted list of delay
 
 from typing import List
 import asyncio
-import random
-
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> List[asyncio.Task]:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """ Description: Take the code from wait_n and alter it into a new function
                  task_wait_n. The code is nearly identical to wait_n except
                  task_wait_random is being called.
     Arguments: n: int, max_delay: int = 10
     """
-    delays = await asyncio.gather(
-        *(task_wait_random(max_delay) for _ in range(n))
-    )
-    for i in range(len(delays)):
-        for j in range(i + 1, len(delays)):
-            if delays[i] > delays[j]:
-                delays[i], delays[j] = delays[j], delays[i]
+    spawn_list = []
+    delay_list = []
+    for i in range(n):
+        delayed_task = task_wait_random(max_delay)
+        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
+        spawn_list.append(delayed_task)
 
-    return delays
+    for spawn in spawn_list:
+        await spawn
+
+    return delay_list
